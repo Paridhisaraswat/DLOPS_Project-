391f5b9618062a2c9869939548336a20ef4e87c2
Paridhi Saraswat <paridhisaraswat06@gmail.com>
2026-05-04 11:14:32 +0530

    Evaluation_completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
 from src.cnnClassifier.entity.config_entity import (
-    DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig
+    DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig,EvaluationConfig
 )
 from pathlib import Path
 import os
@@ -84,3 +84,14 @@ class ConfigurationManager:
             params_learning_rate=params['LEARNING_RATE'] 
 
         )
+    
+
+    def get_validation_config(self) -> EvaluationConfig:
+        return EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params['IMAGE_SIZE'],
+            params_batch_size=self.params['BATCH_SIZE']
+        )
+    
